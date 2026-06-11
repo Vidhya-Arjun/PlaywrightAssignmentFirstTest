61bf8d43967e9539e84c66b897f258cd55944264
Vidhya-Arjun <vidhyasarjun@gmail.com>
2026-06-11 12:43:44 +0530

    optimised code along with comments

--- tests/config-smoke.spec.js
+++ tests/config-smoke.spec.js
@@ -5,30 +5,53 @@ async function openLoginPage(page)
    await page.goto('/login')
 }
 
-test("page title validation",async({page}) =>
+ function getEmailField(page)
 {
+    return page.locator("input#email");
+}
+
+ function getSignInButtonLocator(page)
+{
+    return page.locator("button#login-btn");
+}
+
+test("page title validation",async({page}) =>
+{  
         await openLoginPage(page);
         const title = await page.title();
         await expect(title).toContain("EventHub");
-        const email_field = page.locator("input#email");
-        const signInButton = page.locator("button#login-btn");
-        await email_field.toBeVissible();
-        await signInButton.toBeVissible();
+        const email_field = getEmailField(page);
+        const signInButton = getSignInButtonLocator(page);
+        await email_field.toBeVisible();
+        await signInButton.toBeVisible();
 
 })
 
+
+
 test.only("Compare page fixture and browser context",async({browser,page}) =>
 {
+            // Step 1: Fixture page
+            await openLoginPage(page);
+            const email_field = getEmailField(page);
 
+            await email_field.fill("beginner@sample.com");
+            await expect(email_field).toHaveValue("beginner@sample.com");   
+            
+
+              // Step 2: Isolated context
+
+//difference between page fixture and browser context
+//The page fixture is a ready-to-use browser tab provided automatically by Playwright’s test runner.
+//A separate, isolated browser session inside the same browser instance. Think of it like a new incognito window.
+//Useful when you want multiple independent sessions in the same test (e.g., simulating two different users logging in).
             const isolatedContext = await browser.newContext();
             const isolatedPage = await isolatedContext.newPage();
-            await openLoginPage(page);
-            const email_field = page.locator("input#email");
-            await email_field.fill("beginner@sample.com");
-            await expect(email_field).toHaveValue("beginner@sample.com");    
+           
             await isolatedPage.goto('/login');
             await expect(isolatedPage).toHaveTitle(/EventHub/);
-            const email_field_ip = isolatedPage.locator("input#email");
+            const email_field_ip = getEmailField(isolatedPage);
             await expect(email_field_ip).toBeEmpty();
+           
             await isolatedContext.close();
 }) 
